f7867f06c789868859cf16eba8b403ebf77171ae
ikramkhan <ikramkhan951@gmail.com>
2023-06-11 04:55:30 +0500

    dags added

---
+++ dags/recog_multi_downstream_dag.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def task1():
+    print("Task 1")
+
+def task2():
+    print("Task 2")
+
+def task3():
+    print("Task 3")
+
+def task4():
+    print("Task 4")
+
+def task5():
+    print("Task 5")
+
+def task6():
+    print("Task 6")
+
+default_args = {
+    'owner': 'your_name',
+    'start_date': datetime(2023, 6, 1),
+}
+
+dag = DAG(
+    'multi_downstream_dag',
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+with dag:
+    t1 = PythonOperator(
+        task_id='task1',
+        python_callable=task1,
+    )
+
+    t2 = PythonOperator(
+        task_id='task2',
+        python_callable=task2,
+    )
+
+    t3 = PythonOperator(
+        task_id='task3',
+        python_callable=task3,
+    )
+
+    t4 = PythonOperator(
+        task_id='task4',
+        python_callable=task4,
+    )
+
+    t5 = PythonOperator(
+        task_id='task5',
+        python_callable=task5,
+    )
+
+    t6 = PythonOperator(
+        task_id='task6',
+        python_callable=task6,
+    )
+
+    t1 >> t2
+    t1 >> t3
+    t2 >> t4
+    t3 >> t4
+    t4 >> t5
+    t4 >> t6
